44fcd8dcaec7d70d41451cc9f092be308ca3d686
danglotb <benjamin.danglot@davidson.fr>
2022-06-13 11:26:37 +0200

    feat(xjoules): add read_yaml file

--- src/fr_davidson_diff_xjoules/utils/files_utils.rs
+++ src/fr_davidson_diff_xjoules/utils/files_utils.rs
@@ -1,5 +1,8 @@
 use std::fs;
+
+use serde::de::DeserializeOwned;
 use serde_json::{Map, Value};
+use serde_yaml;
 
 pub fn read_json(path_to_json_file: String) -> Map<String, Value> {
     let json_file_content = fs::read_to_string(path_to_json_file).unwrap();
@@ -7,8 +10,16 @@ pub fn read_json(path_to_json_file: String) -> Map<String, Value> {
     return json_content.as_object().unwrap().clone();
 }
 
+pub fn read_yaml<T: DeserializeOwned>(path_to_yaml_file: String) -> T
+{
+    let yaml_file_content = fs::read_to_string(path_to_yaml_file).unwrap();
+    return serde_yaml::from_str::<T>(&yaml_file_content).unwrap();
+}
+
+
 #[cfg(test)]
 mod tests {
+    use crate::fr_davidson_diff_xjoules::Configuration;
     use super::*;
 
     #[test]
@@ -19,5 +30,13 @@ mod tests {
         let coverage: &Vec<Value> = json_content["fr.davidson.AppTest#testRandomQuickSort"]["src/test/java/fr/davidson/AppTest.java"].as_array().unwrap();
         assert!(coverage[0].is_number());
         assert_eq!(20, coverage.len());
+    }
+
+    #[test]
+    fn test_read_yaml() {
+        let configuration = read_yaml::<Configuration>(String::from("test_resources/configuration_file_example.yaml"));
+        assert_eq!("/tmp/v1", configuration.path_v1);
+        assert_eq!("/tmp/v2", configuration.path_v2);
+        assert_eq!("java -jar my.jar --task coverage --path ${path_project}", configuration.cmd_coverage);
     }
 }
