76ebd8f8d025cf10c3619220d54a691f2c47d338
danglotb <benjamin.danglot@davidson.fr>
2022-06-13 11:28:13 +0200

    feat(xjoules) first implementation of Configuration

--- src/fr_davidson_diff_xjoules.rs
+++ src/fr_davidson_diff_xjoules.rs
@@ -1,1 +1,18 @@
+use serde_derive::Deserialize;
+
+use utils::files_utils::read_yaml;
+
 pub mod utils;
+
+#[derive(Deserialize)]
+pub struct Configuration {
+    pub path_v1: String,
+    pub path_v2: String,
+    pub cmd_coverage: String,
+}
+
+impl Configuration {
+    pub fn new(path_to_configuration_file: String) -> Configuration {
+        return read_yaml::<Configuration>(path_to_configuration_file);
+    }
+}
