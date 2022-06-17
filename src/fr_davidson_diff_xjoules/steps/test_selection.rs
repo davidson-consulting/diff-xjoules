1ce4fd1515680678c83631a81a18947635d120f0
danglotb <benjamin.danglot@davidson.fr>
2022-06-17 11:14:01 +0200

    refactor(xjoules): do not use Configuration as parameter

--- src/fr_davidson_diff_xjoules/steps/test_selection.rs
+++ src/fr_davidson_diff_xjoules/steps/test_selection.rs
@@ -1,17 +1,69 @@
-use crate::fr_davidson_diff_xjoules::{Configuration, SUFFIX_V1, SUFFIX_V2, utils::{coverage::{COVERAGE_FILENAME, run_coverage_cmd}}, DiffXJoulesData};
+use std::fs;
+
+use crate::fr_davidson_diff_xjoules::{Configuration, SUFFIX_V1, SUFFIX_V2, utils::{coverage::{COVERAGE_FILENAME, run_coverage_cmd}, command::{run_command, run_command_redirect_to_file}}, DiffXJoulesData};
+
+const DIFF_FILENAME: &str = "diff";
+
+pub struct TestSelection {
+    pub test_selection: Vec<String>
+}
 
 pub fn run(configuration: Configuration, mut diff_xjoules_data: DiffXJoulesData) {
-    // 1 compute the coverages
+    // diff_xjoules_data = compute_coverages(&configuration, diff_xjoules_data);
+    // diff_xjoules_data.diff = Some(compute_diff(configuration));
+}
+
+fn compute_coverage(configuration: &Configuration, mut diff_xjoules_data: DiffXJoulesData) -> DiffXJoulesData {
     diff_xjoules_data.coverage_v1 = Some(run_coverage_cmd(
-        configuration.path_v1, 
-        configuration.cmd_coverage.clone(),
-        configuration.path_output_coverage.clone() + "/" + COVERAGE_FILENAME + SUFFIX_V1
+        &configuration.path_v1, 
+        &configuration.cmd_coverage,
+        format!("{}/{}{}", &configuration.path_output_dir, COVERAGE_FILENAME, SUFFIX_V1)
     ));
     diff_xjoules_data.coverage_v2 = Some(run_coverage_cmd(
-        configuration.path_v2,
-         configuration.cmd_coverage.clone(),
-         configuration.path_output_coverage.clone() + "/" + COVERAGE_FILENAME + SUFFIX_V2
+        &configuration.path_v2,
+         &configuration.cmd_coverage,
+         format!("{}/{}{}", &configuration.path_output_dir, COVERAGE_FILENAME, SUFFIX_V2)
     ));
-    // 2 run algo on diff
-    // 3 output the selected tests in a JSON (and init a field / struct somewhere)
+    return diff_xjoules_data;
+}
+
+fn compute_diff(path_v1: &str, path_v2: &str, src_folder: &str, path_output_dir: &str) -> String {
+    run_command_redirect_to_file(
+        &format!("diff -r {}/{} {}/{}", path_v1, src_folder, path_v2, src_folder),
+        &format!("{}/{}", path_output_dir, DIFF_FILENAME)
+    );
+    return fs::read_to_string(format!("{}/{}", path_output_dir, DIFF_FILENAME)).unwrap();
+}
+
+fn select_tests(mut diff_xjoules_data: DiffXJoulesData) -> DiffXJoulesData {
+    return diff_xjoules_data;
+}
+
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_run() {
+
+    }
+
+    #[test]
+    fn test_compute_coverages() {
+        
+    }
+
+    #[test]
+    fn test_compute_diff() {
+        compute_diff(
+            "diff-jjoules/src/test/resources/diff-jjoules-toy-java-project/",
+            "diff-jjoules/src/test/resources/diff-jjoules-toy-java-project-v2/",
+            "src/main/java",
+            "target",
+        );
+        let expected_diff_content = "diff -r diff-jjoules/src/test/resources/diff-jjoules-toy-java-project//src/main/java/fr/davidson/App.java diff-jjoules/src/test/resources/diff-jjoules-toy-java-project-v2//src/main/java/fr/davidson/App.java\n19c19\n<             Integer pivot = arr.get(0);\n---\n>             Integer pivot = arr.get(new Random().nextInt(arr.size()));\n";
+        let diff_content = fs::read_to_string(format!("{}/{}", "target", DIFF_FILENAME)).unwrap();
+        assert_eq!(expected_diff_content, diff_content);
+    }
+
 }
