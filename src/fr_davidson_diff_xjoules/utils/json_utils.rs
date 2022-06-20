7a6fc0cbf6b6b0575c29464e560d4e110b958686
danglotb <benjamin.danglot@davidson.fr>
2022-06-20 12:08:32 +0200

    feat(xjoules): write_json

--- src/fr_davidson_diff_xjoules/utils/json_utils.rs
+++ src/fr_davidson_diff_xjoules/utils/json_utils.rs
@@ -1,7 +1,12 @@
 use std::fs;
-use serde::de::DeserializeOwned;
+use serde::{de::DeserializeOwned, Serialize};
 
 const JSON_EXTENSION: &str = ".json";
+
+pub fn write_json<T: Serialize>(path_to_json_file: &str, data: T) {
+    let json_file_content = serde_json::to_string(&data).unwrap();
+    fs::write(format!("{}{}", path_to_json_file, JSON_EXTENSION), json_file_content).unwrap();
+}
 
 pub fn read_json<T: DeserializeOwned>(path_to_json_file: &str) -> T {
     let json_file_content = fs::read_to_string(format!("{}{}", path_to_json_file, JSON_EXTENSION)).unwrap();
@@ -16,6 +21,16 @@ mod tests {
     #[test]
     fn test_read_json() {
         let coverage: Coverage = read_json::<Coverage>("test_resources/coverage");
+        assert_eq!("fr.davidson.AppTest#testRandomQuickSort", coverage.test_coverages.get(0).unwrap().test_identifier);
+        assert_eq!("src/main/java/fr/davidson/App.java", coverage.test_coverages.get(0).unwrap().file_coverages.get(0).unwrap().filename);
+        assert_eq!(21, coverage.test_coverages.get(0).unwrap().file_coverages.get(0).unwrap().covered_lines.len());
+    }
+
+    #[test]
+    fn test_write_json() {
+        let mut coverage: Coverage = read_json::<Coverage>("test_resources/coverage");
+        write_json::<Coverage>("target/copy_coverage", coverage);
+        coverage = read_json::<Coverage>("target/copy_coverage");
         assert_eq!("fr.davidson.AppTest#testRandomQuickSort", coverage.test_coverages.get(0).unwrap().test_identifier);
         assert_eq!("src/main/java/fr/davidson/App.java", coverage.test_coverages.get(0).unwrap().file_coverages.get(0).unwrap().filename);
         assert_eq!(21, coverage.test_coverages.get(0).unwrap().file_coverages.get(0).unwrap().covered_lines.len());
