e68d59af6013736d7ccee4a007c664aa1c7e718e
danglotb <benjamin.danglot@davidson.fr>
2022-06-20 10:55:18 +0200

    feat(xjoules): test_selection implemented

--- src/fr_davidson_diff_xjoules/steps/test_selection.rs
+++ src/fr_davidson_diff_xjoules/steps/test_selection.rs
@@ -1,69 +1,275 @@
-use std::fs;
+use std::{collections::HashSet, fs};
 
-use crate::fr_davidson_diff_xjoules::{Configuration, SUFFIX_V1, SUFFIX_V2, utils::{coverage::{COVERAGE_FILENAME, run_coverage_cmd}, command::{run_command, run_command_redirect_to_file}}, DiffXJoulesData};
+use crate::fr_davidson_diff_xjoules::{
+    utils::{
+        command::{run_command, run_command_redirect_to_file},
+        coverage::{find_test_executing_lines, run_coverage_cmd, Coverage, COVERAGE_FILENAME},
+    },
+    Configuration, DiffXJoulesData, SUFFIX_V1, SUFFIX_V2,
+};
 
 const DIFF_FILENAME: &str = "diff";
 
 pub struct TestSelection {
-    pub test_selection: Vec<String>
+    pub test_selection: HashSet<String>,
 }
 
-pub fn run(configuration: Configuration, mut diff_xjoules_data: DiffXJoulesData) {
-    // diff_xjoules_data = compute_coverages(&configuration, diff_xjoules_data);
-    // diff_xjoules_data.diff = Some(compute_diff(configuration));
+impl TestSelection {
+    pub fn new() -> TestSelection {
+        TestSelection {
+            test_selection: (HashSet::new()),
+        }
+    }
 }
 
-fn compute_coverage(configuration: &Configuration, mut diff_xjoules_data: DiffXJoulesData) -> DiffXJoulesData {
-    diff_xjoules_data.coverage_v1 = Some(run_coverage_cmd(
-        &configuration.path_v1, 
-        &configuration.cmd_coverage,
-        format!("{}/{}{}", &configuration.path_output_dir, COVERAGE_FILENAME, SUFFIX_V1)
+pub fn run(configuration: Configuration, diff_xjoules_data: &mut DiffXJoulesData) {
+    diff_xjoules_data.coverage_v1 = Some(compute_coverage(
+        &configuration.path_v1,
+        &configuration.coverage_cmd,
+        &configuration.path_output_dir,
+        SUFFIX_V1,
     ));
-    diff_xjoules_data.coverage_v2 = Some(run_coverage_cmd(
+    diff_xjoules_data.coverage_v2 = Some(compute_coverage(
         &configuration.path_v2,
-         &configuration.cmd_coverage,
-         format!("{}/{}{}", &configuration.path_output_dir, COVERAGE_FILENAME, SUFFIX_V2)
+        &configuration.coverage_cmd,
+        &configuration.path_output_dir,
+        SUFFIX_V2,
     ));
-    return diff_xjoules_data;
+    diff_xjoules_data.diff = Some(compute_diff(
+        &configuration.path_v1,
+        &configuration.path_v2,
+        &configuration.src_folder,
+        &configuration.path_output_dir,
+    ));
+    diff_xjoules_data.test_selection = Some(select_tests(
+        &configuration.path_v1,
+        diff_xjoules_data.coverage_v1.as_ref().unwrap(),
+        diff_xjoules_data.coverage_v2.as_ref().unwrap(),
+        &diff_xjoules_data.diff.as_ref().unwrap(),
+    ));
+    // TODO write test_selection in a json file
+}
+
+fn compute_coverage(
+    path_to_project: &str,
+    coverage_cmd: &str,
+    path_output_dir: &str,
+    suffix_version: &str,
+) -> Coverage {
+    return run_coverage_cmd(
+        path_to_project,
+        coverage_cmd,
+        format!(
+            "{}/{}{}",
+            path_output_dir, COVERAGE_FILENAME, suffix_version
+        ),
+    );
 }
 
 fn compute_diff(path_v1: &str, path_v2: &str, src_folder: &str, path_output_dir: &str) -> String {
     run_command_redirect_to_file(
-        &format!("diff -r {}/{} {}/{}", path_v1, src_folder, path_v2, src_folder),
-        &format!("{}/{}", path_output_dir, DIFF_FILENAME)
+        &format!(
+            "diff -r {}/{} {}/{}",
+            path_v1, src_folder, path_v2, src_folder
+        ),
+        &format!("{}/{}", path_output_dir, DIFF_FILENAME),
     );
     return fs::read_to_string(format!("{}/{}", path_output_dir, DIFF_FILENAME)).unwrap();
 }
 
-fn select_tests(mut diff_xjoules_data: DiffXJoulesData) -> DiffXJoulesData {
-    return diff_xjoules_data;
+fn select_tests(
+    path_to_project: &str,
+    coverage_v1: &Coverage,
+    coverage_v2: &Coverage,
+    diff: &str,
+) -> TestSelection {
+    let mut test_selection: TestSelection = TestSelection::new();
+    let lines_diff = Vec::from_iter(diff.lines());
+    let mut i = 0;
+    while i < lines_diff.len() {
+        let filename =
+            &lines_diff[i].split_whitespace().nth(2).unwrap()[path_to_project.len() + 1..];
+        i = i + 1;
+        loop {
+            let operation = lines_diff[i];
+            i = i + 1;
+            let mut nb_modified_line = 0;
+            while i < lines_diff.len()
+                && (lines_diff[i].starts_with(">") || lines_diff[i].starts_with("<"))
+            {
+                nb_modified_line = nb_modified_line + 1;
+                i = i + 1;
+            }
+            if operation.contains("a") {
+                test_selection.test_selection.extend(handle_diff_operation(
+                    operation,
+                    "a",
+                    nb_modified_line,
+                    filename,
+                    &coverage_v2,
+                ));
+            } else if operation.contains("d") {
+                test_selection.test_selection.extend(handle_diff_operation(
+                    operation,
+                    "d",
+                    nb_modified_line,
+                    filename,
+                    &coverage_v1,
+                ));
+            } else if operation.contains("c") {
+                test_selection.test_selection.extend(handle_diff_operation(
+                    operation,
+                    "c",
+                    nb_modified_line,
+                    filename,
+                    &coverage_v1,
+                ));
+                test_selection.test_selection.extend(handle_diff_operation(
+                    operation,
+                    "c",
+                    nb_modified_line,
+                    filename,
+                    &coverage_v2,
+                ));
+            }
+            if i >= lines_diff.len() || lines_diff[i].starts_with("diff -r") {
+                break;
+            }
+        }
+    }
+    return test_selection;
 }
 
+fn handle_diff_operation(
+    full_operation: &str,
+    operation: &str,
+    nb_modified_line: i16,
+    filename: &str,
+    coverage: &Coverage,
+) -> HashSet<String> {
+    let starting_line = full_operation
+        .split(operation)
+        .nth(0)
+        .unwrap()
+        .parse()
+        .unwrap();
+    let modified_lines: Vec<i16> = (starting_line..starting_line + nb_modified_line).collect();
+    return find_test_executing_lines(coverage, filename, &modified_lines);
+}
 
 mod tests {
     use super::*;
+    use crate::fr_davidson_diff_xjoules::utils::json_utils;
 
     #[test]
     fn test_run() {
-
+        run_command("cp test_resources/coverage_v2.json target/coverage_v2.json");
+        let configuration = Configuration {
+            path_v1: String::from("diff-jjoules/src/test/resources/diff-jjoules-toy-java-project"),
+            path_v2: String::from(
+                "diff-jjoules/src/test/resources/diff-jjoules-toy-java-project-v2",
+            ),
+            src_folder: String::from("src/main/java"),
+            path_output_dir: String::from("target"),
+            coverage_cmd: String::from(
+                "cp test_resources/coverage_v1.json target/coverage_v1.json",
+            ),
+        };
+        let mut diff_xjoules_data = DiffXJoulesData::new();
+        run(configuration, &mut diff_xjoules_data);
+        let expected_diff_content = fs::read_to_string("test_resources/expected_diff").unwrap();
+        assert_eq!(
+            9,
+            diff_xjoules_data.coverage_v1.unwrap().test_coverages.len()
+        );
+        assert_eq!(
+            9,
+            diff_xjoules_data.coverage_v2.unwrap().test_coverages.len()
+        );
+        assert_eq!(expected_diff_content, diff_xjoules_data.diff.unwrap());
+        assert_eq!(
+            4,
+            diff_xjoules_data
+                .test_selection
+                .unwrap()
+                .test_selection
+                .len()
+        );
     }
 
     #[test]
-    fn test_compute_coverages() {
-        
+    fn test_compute_coverage() {
+        let coverage = compute_coverage(
+            "diff-jjoules/src/test/resources/diff-jjoules-toy-java-project/",
+            "cp test_resources/coverage.json target/coverage_v1.json",
+            "target/",
+            SUFFIX_V1,
+        );
+        assert_eq!(
+            "fr.davidson.AppTest#testRandomQuickSort",
+            coverage.test_coverages.get(0).unwrap().test_identifier
+        );
+        assert_eq!(
+            "src/main/java/fr/davidson/App.java",
+            coverage
+                .test_coverages
+                .get(0)
+                .unwrap()
+                .file_coverages
+                .get(0)
+                .unwrap()
+                .filename
+        );
+        assert_eq!(
+            21,
+            coverage
+                .test_coverages
+                .get(0)
+                .unwrap()
+                .file_coverages
+                .get(0)
+                .unwrap()
+                .covered_lines
+                .len()
+        );
     }
 
     #[test]
     fn test_compute_diff() {
         compute_diff(
-            "diff-jjoules/src/test/resources/diff-jjoules-toy-java-project/",
-            "diff-jjoules/src/test/resources/diff-jjoules-toy-java-project-v2/",
+            "diff-jjoules/src/test/resources/diff-jjoules-toy-java-project",
+            "diff-jjoules/src/test/resources/diff-jjoules-toy-java-project-v2",
             "src/main/java",
             "target",
         );
-        let expected_diff_content = "diff -r diff-jjoules/src/test/resources/diff-jjoules-toy-java-project//src/main/java/fr/davidson/App.java diff-jjoules/src/test/resources/diff-jjoules-toy-java-project-v2//src/main/java/fr/davidson/App.java\n19c19\n<             Integer pivot = arr.get(0);\n---\n>             Integer pivot = arr.get(new Random().nextInt(arr.size()));\n";
+        let expected_diff_content = fs::read_to_string("test_resources/expected_diff").unwrap();
         let diff_content = fs::read_to_string(format!("{}/{}", "target", DIFF_FILENAME)).unwrap();
         assert_eq!(expected_diff_content, diff_content);
     }
 
+    #[test]
+    fn test_select_tests() {
+        let coverage_v1: Coverage = json_utils::read_json::<Coverage>("test_resources/coverage_v1");
+        let coverage_v2: Coverage = json_utils::read_json::<Coverage>("test_resources/coverage_v2");
+        let diff = fs::read_to_string("test_resources/expected_diff").unwrap();
+        let test_selection = select_tests(
+            "diff-jjoules/src/test/resources/diff-jjoules-toy-java-project",
+            &coverage_v1,
+            &coverage_v2,
+            &diff,
+        );
+        assert_eq!(4, test_selection.test_selection.len());
+        assert!(test_selection
+            .test_selection
+            .contains(&String::from("fr.davidson.AppTest#testAddedStatement")));
+        assert!(test_selection
+            .test_selection
+            .contains(&String::from("fr.davidson.AppTest#testRemovedStatement")));
+        assert!(test_selection
+            .test_selection
+            .contains(&String::from("fr.davidson.AppTest#testUpdatedStatement")));
+        assert!(test_selection.test_selection.contains(&String::from(
+            "fr.davidson.AppTest#testAddedAndRemovedStatement"
+        )));
+    }
 }
