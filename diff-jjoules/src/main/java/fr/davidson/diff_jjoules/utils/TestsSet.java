44453881c975971b3cf3161fa6f4ce1f752d7359
danglotb <benjamin.danglot@davidson.fr>
2022-06-20 14:03:17 +0200

    feat(jjoules): add TestsSet type

---
+++ diff-jjoules/src/main/java/fr/davidson/diff_jjoules/utils/TestsSet.java
@@ -0,0 +1,11 @@
+package fr.davidson.diff_jjoules.utils;
+
+import java.util.HashSet;
+
+/**
+ * @author Benjamin DANGLOT
+ * benjamin.danglot@davidson.fr
+ * on 20/06/2022
+ */
+public class TestsSet extends HashSet<String> {
+}
