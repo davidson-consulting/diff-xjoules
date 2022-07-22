cb8a6d99e255de840b1400f1e764338177c95010
danglotb <benjamin.danglot@davidson.fr>
2022-07-22 12:20:10 +0200

    test(jjoules): fix CloverReaderTest oracles

--- diff-jjoules/src/test/java/fr/davidson/diff_jjoules/tasks/test_coverage/clover/CloverReaderTest.java
+++ diff-jjoules/src/test/java/fr/davidson/diff_jjoules/tasks/test_coverage/clover/CloverReaderTest.java
@@ -19,16 +19,16 @@ public class CloverReaderTest {
         // The keys used for the filename are a bit weird because we load from src/test/resources and not from the root path of the project
         final Coverage coverage = new CloverReader().read("src/test/resources/diff-jjoules-toy-java-project/src/test/resources/");
         assertTrue(coverage.containsTestIdentifier("fr.davidson.AppTest#testRandom"));
-        assertTrue(coverage.findByTestIdentifier("fr.davidson.AppTest#testRandom").containsFileCoverage("/diff-jjoules-toy-java-project/src/main/java/fr/davidson/App.java"));
+        assertTrue(coverage.findByTestIdentifier("fr.davidson.AppTest#testRandom").containsFileCoverage("s/diff-jjoules-toy-java-project/src/main/java/fr/davidson/App.java"));
         final Integer[] expectedCoveredLines = new Integer[]{10, 11};
         assertEquals(expectedCoveredLines.length,
                 coverage.findByTestIdentifier("fr.davidson.AppTest#testRandom")
-                        .findFileCoverageByFilename("/diff-jjoules-toy-java-project/src/main/java/fr/davidson/App.java")
+                        .findFileCoverageByFilename("s/diff-jjoules-toy-java-project/src/main/java/fr/davidson/App.java")
                         .covered_lines.size());
         for (Integer expectedCoveredLine : expectedCoveredLines) {
             assertTrue(
                     coverage.findByTestIdentifier("fr.davidson.AppTest#testRandom")
-                            .findFileCoverageByFilename("/diff-jjoules-toy-java-project/src/main/java/fr/davidson/App.java")
+                            .findFileCoverageByFilename("s/diff-jjoules-toy-java-project/src/main/java/fr/davidson/App.java")
                             .covered_lines
                             .contains(expectedCoveredLine)
             );
