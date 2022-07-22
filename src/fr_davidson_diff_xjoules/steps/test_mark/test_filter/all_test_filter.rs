bcace500a9c5455d9a818616cf23b7a82d1a8310
danglotb <benjamin.danglot@davidson.fr>
2022-07-22 21:28:16 +0200

    test(xjoules): add test on AllTestFilter filter function

--- src/fr_davidson_diff_xjoules/steps/test_mark/test_filter/all_test_filter.rs
+++ src/fr_davidson_diff_xjoules/steps/test_mark/test_filter/all_test_filter.rs
@@ -24,3 +24,48 @@ impl AllTestFilter {
         AllTestFilter {}
     }
 }
+
+#[cfg(test)]
+mod test {
+    use crate::fr_davidson_diff_xjoules::{
+        configuration::Configuration,
+        diff_data::DiffXJoulesData,
+        measure::version_measure::VersionMeasure,
+        steps::{test_mark::{
+            mark_strategy::MarkStrategyEnum,
+            test_filter::{TestFilter, TestFilterEnum},
+        }, test_selection::TestSelection},
+        utils::json_utils,
+    };
+
+    #[test]
+    fn test_filter() {
+        let test_filter = TestFilterEnum::ALL.get();
+        let configuration = Configuration {
+            path_v1: String::from("diff-jjoules/src/test/resources/diff-jjoules-toy-java-project"),
+            path_v2: String::from(
+                "diff-jjoules/src/test/resources/diff-jjoules-toy-java-project-v2",
+            ),
+            src_folder: String::from("src/main/java"),
+            path_output_dir: String::from("target"),
+            coverage_cmd: String::from(""),
+            instrumentation_cmd: String::from(""),
+            execution_cmd: String::from(""),
+            iteration_warmup: 1,
+            iteration_run: 3,
+            time_to_wait_in_millis: 500,
+            test_filter: TestFilterEnum::ALL,
+            mark_strategy: MarkStrategyEnum::STRICT,
+            indicator_to_consider_for_marking: String::from("UNHALTED_REFERENCE_CYCLES"),
+        };
+        let mut data = DiffXJoulesData::new();
+        data.delta = json_utils::read_json::<VersionMeasure>("test_resources/delta.json");
+        data.test_selection = json_utils::read_json::<TestSelection>("test_resources/test_filter_selection.json");
+        let test_selection = test_filter.filter(&configuration, &data);
+        assert_eq!(4, test_selection.test_selection.len());
+        assert!(test_selection.test_selection.contains("fr.davidson.AppTest#testAddedStatement"));
+        assert!(test_selection.test_selection.contains("fr.davidson.AppTest#testAddedAndRemovedStatement"));
+        assert!(test_selection.test_selection.contains("fr.davidson.AppTest#testUpdatedStatement"));
+        assert!(test_selection.test_selection.contains("fr.davidson.AppTest#testRemovedStatement"));
+    }
+}
