a77b859ae72f3be95070dcfa8b25e93d3caafe4b
danglotb <benjamin.danglot@davidson.fr>
2022-07-24 14:42:35 +0200

    test(xjoules): testing of test_delta step's functions

--- src/fr_davidson_diff_xjoules/steps/test_delta.rs
+++ src/fr_davidson_diff_xjoules/steps/test_delta.rs
@@ -81,3 +81,71 @@ fn compute_median_and_delta_indicator(
         value: median_v2 - median_v1,
     });
 }
+
+#[cfg(test)]
+mod test {
+    use crate::fr_davidson_diff_xjoules::{
+        configuration::Configuration,
+        diff_data::DiffXJoulesData,
+        steps::{test_mark::{mark_strategy::MarkStrategyEnum, test_filter::TestFilterEnum}, test_selection::TestSelection},
+    };
+
+    use super::*;
+
+    #[test]
+    fn test_compute_median_and_delta_indicator() {
+        let mut medians_v1 = Vec::<Data>::new();
+        let mut medians_v2 = Vec::<Data>::new();
+        let mut deltas = Vec::<Data>::new();
+        let data_v1 = json_utils::read_json::<VersionMeasure>("test_resources/data_v1.json");
+        let data_v2 = json_utils::read_json::<VersionMeasure>("test_resources/data_v2.json");
+        let test_measure_v1 = data_v1.find_test_measure("fr.davidson.AppTest#testAddedStatement").unwrap();
+        let test_measure_v2 = data_v2.find_test_measure("fr.davidson.AppTest#testAddedStatement").unwrap();
+        compute_median_and_delta_indicator(&mut medians_v1, &mut medians_v2, &mut deltas, test_measure_v1, test_measure_v2, "duration");
+        compute_median_and_delta_indicator(&mut medians_v1, &mut medians_v2, &mut deltas, test_measure_v1, test_measure_v2, "RAPL_ENERGY_PKG");
+        assert_eq!(136359.0, medians_v1.get(0).unwrap().value);
+        assert_eq!(142499.0, medians_v2.get(0).unwrap().value);
+        assert_eq!(6140.0, deltas.get(0).unwrap().value);
+        assert_eq!("duration", medians_v1.get(0).unwrap().indicator);
+        assert_eq!("duration", medians_v2.get(0).unwrap().indicator);
+        assert_eq!("duration", deltas.get(0).unwrap().indicator);
+        assert_eq!(3580.0, medians_v1.get(1).unwrap().value);
+        assert_eq!(4231.0, medians_v2.get(1).unwrap().value);
+        assert_eq!(651.0, deltas.get(1).unwrap().value);
+        assert_eq!("RAPL_ENERGY_PKG", medians_v1.get(1).unwrap().indicator);
+        assert_eq!("RAPL_ENERGY_PKG", medians_v2.get(1).unwrap().indicator);
+        assert_eq!("RAPL_ENERGY_PKG", deltas.get(1).unwrap().indicator);
+    }
+
+    #[test]
+    fn test_run() {
+        let configuration = Configuration {
+            path_v1: String::from("diff-jjoules/src/test/resources/diff-jjoules-toy-java-project"),
+            path_v2: String::from(
+                "diff-jjoules/src/test/resources/diff-jjoules-toy-java-project-v2",
+            ),
+            src_folder: String::from("src/main/java"),
+            path_output_dir: String::from("target"),
+            coverage_cmd: String::from(""),
+            instrumentation_cmd: String::from(""),
+            execution_cmd: String::from(""),
+            iteration_warmup: 1,
+            iteration_run: 3,
+            time_to_wait_in_millis: 500,
+            test_filter: TestFilterEnum::ALL,
+            mark_strategy: MarkStrategyEnum::STRICT,
+            indicator_to_consider_for_marking: String::from("cycles"),
+        };
+        let mut diff_xjoules_data = DiffXJoulesData::new();
+        diff_xjoules_data.test_selection = json_utils::read_json::<TestSelection>("test_resources/test_filter_selection.json");
+        diff_xjoules_data.data_v1 = json_utils::read_json::<VersionMeasure>("test_resources/data_v1.json");
+        diff_xjoules_data.data_v2 = json_utils::read_json::<VersionMeasure>("test_resources/data_v2.json");
+        assert_eq!(0, diff_xjoules_data.delta.test_measures.len());
+        assert_eq!(0, diff_xjoules_data.median_v1.test_measures.len());
+        assert_eq!(0, diff_xjoules_data.median_v2.test_measures.len());
+        run(&configuration, &mut diff_xjoules_data);
+        assert_eq!(4, diff_xjoules_data.delta.test_measures.len());
+        assert_eq!(4, diff_xjoules_data.median_v1.test_measures.len());
+        assert_eq!(4, diff_xjoules_data.median_v2.test_measures.len());
+    }
+}
