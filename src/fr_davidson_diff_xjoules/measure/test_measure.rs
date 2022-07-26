f8dd546e1f3c97a13b1a7066c21a3d5e97ab87ef
danglotb <benjamin.danglot@davidson.fr>
2022-07-26 16:06:27 +0200

    feat(xjoules): add get_all_measures, get_max and get_min from a given indicator

--- src/fr_davidson_diff_xjoules/measure/test_measure.rs
+++ src/fr_davidson_diff_xjoules/measure/test_measure.rs
@@ -18,8 +18,8 @@ impl TestMeasure {
             .for_each(|data| indicators.push(&data.indicator));
         return indicators;
     }
-    pub fn get_median(&self, indicator: &str) -> f64 {
-        let mut values: Vec<f64> = self
+    pub fn get_all_measures(&self, indicator: &str) -> Vec<f64> {
+        return self
             .measures
             .iter()
             .map(|datas| {
@@ -30,7 +30,15 @@ impl TestMeasure {
                     .value
             })
             .collect();
-        return math::median(&mut values);
+    }
+    pub fn get_median(&self, indicator: &str) -> f64 {
+        return math::median(&mut self.get_all_measures(indicator));
+    }
+    pub fn get_max(&self, indicator: &str) -> f64 {
+        return math::max(&mut self.get_all_measures(indicator));
+    }
+    pub fn get_min(&self, indicator: &str) -> f64 {
+        return math::min(&mut self.get_all_measures(indicator));
     }
 }
 
@@ -39,44 +47,15 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_test_measure_get_all_measures() {
+        let test_measure_test_1 = build_test_measure_for_test();
+        let measures = test_measure_test_1.get_all_measures("cycles");
+        assert_eq!(3, measures.len());
+    }
+
+    #[test]
     fn test_test_measure_get_all_indicators() {
-        let mut test_measure_test_1: TestMeasure = TestMeasure {
-            test_identifier: String::from("test1"),
-            measures: Vec::new(),
-        };
-        let mut data_1 = Vec::new();
-        data_1.push(Data {
-            indicator: String::from("instructions"),
-            value: 20.0,
-        });
-        data_1.push(Data {
-            indicator: String::from("cycles"),
-            value: 2000.0,
-        });
-        test_measure_test_1.measures.push(data_1);
-
-        let mut data_2 = Vec::new();
-        data_2.push(Data {
-            indicator: String::from("instructions"),
-            value: 10.0,
-        });
-        data_2.push(Data {
-            indicator: String::from("cycles"),
-            value: 1000.0,
-        });
-        test_measure_test_1.measures.push(data_2);
-
-        let mut data_3 = Vec::new();
-        data_3.push(Data {
-            indicator: String::from("instructions"),
-            value: 30.0,
-        });
-        data_3.push(Data {
-            indicator: String::from("cycles"),
-            value: 3000.0,
-        });
-        test_measure_test_1.measures.push(data_3);
-
+        let test_measure_test_1 = build_test_measure_for_test();
         let indicators = test_measure_test_1.get_all_indicators();
         assert_eq!(2, indicators.len());
         assert!(indicators.contains(&&String::from("cycles")));
@@ -85,6 +64,26 @@ mod tests {
 
     #[test]
     fn test_test_measure_get_median() {
+        let test_measure_test_1 = build_test_measure_for_test();
+        assert_eq!(20.0, test_measure_test_1.get_median("instructions"));
+        assert_eq!(2000.0, test_measure_test_1.get_median("cycles"));
+    }
+
+    #[test]
+    fn test_test_measure_get_max() {
+        let test_measure_test_1 = build_test_measure_for_test();
+        assert_eq!(30.0, test_measure_test_1.get_max("instructions"));
+        assert_eq!(3000.0, test_measure_test_1.get_max("cycles"));
+    }
+
+    #[test]
+    fn test_test_measure_get_min() {
+        let test_measure_test_1 = build_test_measure_for_test();
+        assert_eq!(10.0, test_measure_test_1.get_min("instructions"));
+        assert_eq!(1000.0, test_measure_test_1.get_min("cycles"));
+    }
+
+    fn build_test_measure_for_test() -> TestMeasure {
         let mut test_measure_test_1: TestMeasure = TestMeasure {
             test_identifier: String::from("test1"),
             measures: Vec::new(),
@@ -122,7 +121,6 @@ mod tests {
         });
         test_measure_test_1.measures.push(data_3);
 
-        assert_eq!(20.0, test_measure_test_1.get_median("instructions"));
-        assert_eq!(2000.0, test_measure_test_1.get_median("cycles"));
+        return test_measure_test_1;
     }
 }
