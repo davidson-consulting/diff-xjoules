f467cd1d6da99cdf42e2a196c72e4a19d363d9cb
danglotb <benjamin.danglot@davidson.fr>
2022-08-09 21:47:50 +0200

    test(jjoules): remove instrumentation

--- diff-jjoules/src/test/resources/diff-jjoules-toy-java-project-v2/src/test/java/fr/davidson/AppTest.java
+++ diff-jjoules/src/test/resources/diff-jjoules-toy-java-project-v2/src/test/java/fr/davidson/AppTest.java
@@ -1,71 +1,74 @@
 package fr.davidson;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
 public class AppTest {
-    @org.junit.Test
+
+    @Test
     public void testAddedStatement() {
-        fr.davidson.tlpc.sensor.TLPCSensor.start("fr.davidson.AppTest#testAddedStatement");
-        new fr.davidson.App().addedStatement();
-        fr.davidson.tlpc.sensor.TLPCSensor.stop("fr.davidson.AppTest#testAddedStatement");
+        new App().addedStatement();
     }
 
-    @org.junit.Test
+    @Test
     public void testRemovedStatement() {
-        fr.davidson.tlpc.sensor.TLPCSensor.start("fr.davidson.AppTest#testRemovedStatement");
-        new fr.davidson.App().removedStatement();
-        fr.davidson.tlpc.sensor.TLPCSensor.stop("fr.davidson.AppTest#testRemovedStatement");
+        new App().removedStatement();
     }
 
-    @org.junit.Test
+    @Test
     public void testUpdatedStatement() {
-        fr.davidson.tlpc.sensor.TLPCSensor.start("fr.davidson.AppTest#testUpdatedStatement");
-        new fr.davidson.App().updatedStatement();
-        fr.davidson.tlpc.sensor.TLPCSensor.stop("fr.davidson.AppTest#testUpdatedStatement");
+        new App().updatedStatement();
     }
 
-    @org.junit.Test
+    @Test
     public void testAddedAndRemovedStatement() {
-        fr.davidson.tlpc.sensor.TLPCSensor.start("fr.davidson.AppTest#testAddedAndRemovedStatement");
-        new fr.davidson.App().addedAndRemovedStatement();
-        fr.davidson.tlpc.sensor.TLPCSensor.stop("fr.davidson.AppTest#testAddedAndRemovedStatement");
+        new App().addedAndRemovedStatement();
     }
 
-    @org.junit.Test
+    @Test
     public void testEmpty() {
-        final java.util.List<java.lang.Integer> emptyList = java.util.Collections.emptyList();
-        fr.davidson.App.sort(emptyList);
-        org.junit.Assert.assertTrue(emptyList.isEmpty());
+        final List<Integer> emptyList = Collections.emptyList();
+        App.sort(emptyList);
+        assertTrue(emptyList.isEmpty());
     }
 
-    @org.junit.Test
+    @Test
     public void testSingleton() {
-        final java.util.List<java.lang.Integer> singletonList = java.util.Collections.singletonList(23);
-        fr.davidson.App.sort(singletonList);
-        org.junit.Assert.assertTrue(fr.davidson.AppTest.isSorted(singletonList));
+        final List<Integer> singletonList = Collections.singletonList(23);
+        App.sort(singletonList);
+        assertTrue(isSorted(singletonList));
     }
 
-    @org.junit.Test
+    @Test
     public void testRandom() {
-        final java.util.List<java.lang.Integer> randomList = getRandomList(100);
-        fr.davidson.App.sort(randomList);
-        org.junit.Assert.assertTrue(fr.davidson.AppTest.isSorted(randomList));
+        final List<Integer> randomList = getRandomList(100);
+        App.sort(randomList);
+        assertTrue(isSorted(randomList));
     }
 
-    @org.junit.Test
+    @Test
     public void testRandomQuickSort() {
-        final java.util.List<java.lang.Integer> randomList = getRandomList(100);
-        final java.util.List<java.lang.Integer> sortedRandomList = fr.davidson.App.quickSort(randomList);
-        org.junit.Assert.assertTrue(fr.davidson.AppTest.isSorted(sortedRandomList));
+        final List<Integer> randomList = getRandomList(100);
+        final List<Integer> sortedRandomList = App.quickSort(randomList);
+        assertTrue(isSorted(sortedRandomList));
     }
 
-    @org.junit.Test
+    @Test
     public void testRandomQuickSortLarge() {
-        final java.util.List<java.lang.Integer> randomList = getRandomList(100000);
-        final java.util.List<java.lang.Integer> sortedRandomList = fr.davidson.App.quickSort(randomList);
-        org.junit.Assert.assertTrue(fr.davidson.AppTest.isSorted(sortedRandomList));
+        final List<Integer> randomList = getRandomList(100000);
+        final List<Integer> sortedRandomList = App.quickSort(randomList);
+        assertTrue(isSorted(sortedRandomList));
     }
 
-    private java.util.List<java.lang.Integer> getRandomList(final int bound) {
-        final java.util.List<java.lang.Integer> randomList = new java.util.ArrayList<>();
-        final java.util.Random random = new java.util.Random();
+    private List<Integer> getRandomList(final int bound) {
+        final List<Integer> randomList = new ArrayList<>();
+        final Random random = new Random();
         final int nbRandomElement = random.nextInt(bound);
         for (int i = 0; i < nbRandomElement; i++) {
             randomList.add(random.nextInt());
@@ -73,7 +76,7 @@ public class AppTest {
         return randomList;
     }
 
-    private static boolean isSorted(java.util.List<java.lang.Integer> list) {
+    private static boolean isSorted(List<Integer> list) {
         int previous = list.get(0);
         for (int i = 1; i < list.size(); i++) {
             int current = list.get(i);
@@ -83,9 +86,5 @@ public class AppTest {
             previous = current;
         }
         return true;
-    }
-
-    static {
-           java.lang.Runtime.getRuntime().addShutdownHook(       new java.lang.Thread() {           @java.lang.Override           public void run() {               fr.davidson.tlpc.sensor.TLPCSensor.report(                   "/home/benjamin/workspace/diff-xjoules/diff-jjoules/src/test/resources/diff-jjoules-toy-java-project-v2/diff-measurements/measurements.json"               );           }       }   );
     }
 }
