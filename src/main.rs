8367713d4ab2a224193718822af6bbe19e4963a4
danglotb <benjamin.danglot@davidson.fr>
2022-08-22 15:00:01 +0200

    feat(xjoules): improve command line documentation

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,11 @@ pub mod fr_davidson_diff_xjoules;
 use clap::Parser;
 
 #[derive(Parser)]
+#[clap(author, version, about)]
+/// Rust Plugin for Diff-XJoules to measure the impact of commits on energy consumption exploiting existing test developers
 struct Cli {
     #[clap(parse(from_os_str))]
+    /// Path to the YAML configuration file
     yaml_path: std::path::PathBuf,
 }
 
